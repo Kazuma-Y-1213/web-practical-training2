ce838e9963242d477797aca434413f0f392501a9
Kazuma-Y-1213 <m2520036@ngo.ohara.ac.jp>
2026-02-02 13:47:09 +0900

    aaa.htmlにひとまとめにする途中 indexなどの頭にファイル名のメモ書きした ハンバーガーメニュ実装途中

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
-const hamburgerMenus = document.querySelectorAll(".hamburger-menu");
-console.log(hamburgerMenus);
+const body = document.querySelector("body");
+const comStyleBody = getComputedStyle(body);
+const main = document.querySelector("main");
+const comStyleMain = getComputedStyle(main);
+const header = document.querySelector("header");
+const comStyleHeader = getComputedStyle(header);
+
+
+
+
+console.log(header);
+
+const gotElementsList = Array(body,main,header);
+
+// hamburgerMenu
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+console.log(hamburgerMenu);
+
+console.log(hamburgerMenu,"hamburgerMenu");
+
+hamburgerMenu.style.width = body.clientWidth + "px";
+const headerMarginTop = header.clientHeight
+hamburgerMenu.style.height = main.clientHeight - (header.width + header.style.marginTop + header.style.marginBottom)  + "px";
+console.log(header.clientWidth ,comStyleHeader.marginTop , comStyleHeader.marginBottom , "header width");
+const bodyMarginLeft = (window.innerWidth - body.clientWidth) / 2;
+hamburgerMenu.style.marginLeft = bodyMarginLeft + "px";
+// -------------
