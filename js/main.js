dab3005e73564efbae4b4ab531db3692bd73b7d7
Kazuma-Y-1213 <m2520036@ngo.ohara.ac.jp>
2026-02-19 11:45:59 +0900

    ハンバーガメニュのアニメーション制作完了

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 const body = document.querySelector("body");
 const comStyleBody = getComputedStyle(body);
 const main = document.querySelector("main");
@@ -16,7 +17,7 @@ const hamburgerMenu = document.querySelector("#hamburger-menu");
 const comStyleHamMenu = getComputedStyle(hamburgerMenu);
 const hamburgerBackground = document.querySelector("#hamburger-background");
 const comStyleHamBg = getComputedStyle(hamburgerBackground);
-
+//npm install -p typescript kokomadeyatta.
 // origin setting
 for (let i = 0; i < hamBtnLineParents.length; i++) {
   hamBtnLineParents[i].children[0].style.transformOrigin = "right";
@@ -29,85 +30,94 @@ console.log(hamBtnLineParents[1].children[0].getBoundingClientRect().top);
 
 //-------
 const hamLineScale = 5;
-const openHamburgerScaleAnimation = {
+const shrinkHamLineScaleKeyframes = {
   scale: ["100% 100%", `${hamLineScale}% 100%`],
   opacity: ["100%", "100%"],
 };
-const closeHamburgerScaleAnimation = {
+const extendHamLineScaleKeyframes = {
   scale: [`${hamLineScale}% 100%`, "100% 100%"],
   opacity: ["100%", "100%"],
 };
-console.log(openHamburgerScaleAnimation);
+console.log(shrinkHamLineScaleKeyframes);
 const hamLineMove =
   hamBtnLineParents[1].children[0].getBoundingClientRect().top -
   hamBtnLineParents[0].children[0].getBoundingClientRect().top;
-const openHamburgerDownAnimation = {
+const moveDownHamLineKeyframesOpen = {
   translate: ["100% 100%", `0% ${hamLineMove}px`],
 };
-const openHamburgerUpAnimation = {
+const moveUpHamLineKeyframesOpen = {
   translate: ["100% 100%", `0% -${hamLineMove}px`],
 };
 
-const closeHamburgerDownAnimation = {
+const moveDownHamLineKeyframesClose = {
   translate: [`0% -${hamLineMove}px`, "0"],
 };
-const closeHamburgerUpAnimation = {
+const moveUpHamLineKeyframesClose = {
   translate: [`0% ${hamLineMove}px`, "0"],
 };
 
-const hamOpDuraTime = 1000;
-const hamburgerAnimationOption = {
+const hamOpDuraTime = 250;
+const hamLineAnimationOption = {
   duration: hamOpDuraTime,
   fill: "forwards",
-  ease: "line",
+  ease: "ease-out",
 };
-const hamburgerAnimationOptionHalfDuration = {
+const hamLineAnimationOptionHalf = {
   duration: hamOpDuraTime / 2,
   fill: "forwards",
-  ease: "line",
+  ease: "ease-out",
 };
 
 //--------
 //test
-{
+// openHamBtnAnimate
+/*
+  中間の線が中心に向かって縮む ---- 0.5
+
+  1.*0.0*
+    上下の線が中心に向かって縮む ---- 0.5
+  2.*0.5*
+    その後中心に集まるために上下に動く -----0.5
+  3.*1.0*
+    上下の線の角度が変わる ---- 0.0
+    100%まで伸びる ---- 0.5
+
+*/
   function openHamBtnAnimate() {
     console.log("in openHamFunction");
     for (let i = 0; i < hamBtnLineParents.length; i++) {
       for (let j = 0; j < hamBtnLineParents[0].children.length; j++) {
         if ((i + 1) % 2 == 0) {
-          console.log(`In openHam-If`);
-
-          // ------ 1 --------
+          
           hamBtnLineParents[i].children[j].animate(
-            openHamburgerScaleAnimation,
-            hamburgerAnimationOption,
+            shrinkHamLineScaleKeyframes,
+            hamLineAnimationOptionHalf,
           );
         } else {
+          // ------ 1 --------
           hamBtnLineParents[i].children[j].animate(
-            openHamburgerScaleAnimation,
-            hamburgerAnimationOptionHalfDuration,
+            shrinkHamLineScaleKeyframes,
+            hamLineAnimationOptionHalf,
           );
-
-          // ------- 2 --------
-
+          // ------ 2 --------
           setTimeout(() => {
             hamBtnLineParents[0].children[j].animate(
-              openHamburgerDownAnimation,
-              hamburgerAnimationOptionHalfDuration,
+              moveDownHamLineKeyframesOpen,
+              hamLineAnimationOptionHalf,
             );
 
             hamBtnLineParents[2].children[j].animate(
-              openHamburgerUpAnimation,
-              hamburgerAnimationOptionHalfDuration,
+              moveUpHamLineKeyframesOpen,
+              hamLineAnimationOptionHalf,
             );
           }, hamOpDuraTime / 2);
-
-          // --------- 3 ---------
+          // ------- 3 --------
+          
 
           setTimeout(() => {
+            // 角度 -----
             hamBtnLineParents[i].children[j].style.rotate = "45deg";
-            //右上と左下だけ斜め右上に向ける
-            if (Boolean(i) ^ Boolean(j)) {
+            if (Boolean(i) ^ Boolean(j)) {//右上と左下だけ斜め右上に向ける
               hamBtnLineParents[i].children[j].style.rotate = "-45deg";
             }
             //-----------
@@ -115,55 +125,80 @@ const hamburgerAnimationOptionHalfDuration = {
               {
                 scale: ["100%"],
               },
-              hamburgerAnimationOption,
+              hamLineAnimationOptionHalf,
             );
           }, hamOpDuraTime);
         }
       }
     }
   }
+  /*
+  
+  1.*0.0*
+    斜めになった上下の線を縮める ---- 0.5
+    
+  2.*0.5*
+    上下の線の角度を0degに戻す ---- 0.0
+    上下に移動させる ---- 0.5
+    中間の線を100%まで伸ばす ---- 0.5
+    
+  3.*1.0*
+    上下の線を伸ばす ---- 0.5
+    
+
+  */
   function closeHamBtnAnimate() {
     for (let i = 0; i < hamBtnLineParents.length; i++) {
       for (let j = 0; j < hamBtnLineParents[0].children.length; j++) {
         if ((i + 1) % 2 == 0) {
           //真ん中の線だけ
-          // -------- 1 ----------
-
-          hamBtnLineParents[i].children[j].animate(
-            {
-              scale: ["100%"],
-            },
-            hamburgerAnimationOption,
+          setTimeout(()=>{
+              hamBtnLineParents[i].children[j].animate(
+                {
+                  scale: ["100%"],
+                },
+                hamLineAnimationOptionHalf,
+              );
+          }
+          ,hamOpDuraTime /2
           );
-
           console.log("close center");
         } else {
-          hamBtnLineParents[i].children[j].style.rotate = "0deg";
-
-          hamBtnLineParents[i].children[j].style.scale =
-            `${hamLineScale}% 100%`;
-
-          console.log(hamBtnLineParents[i].children[j].style.scale, i, j);
-          hamBtnLineParents[0].children[j].animate(
-            closeHamburgerUpAnimation,
-            hamburgerAnimationOptionHalfDuration,
+          
+          // ------ 1 ------
+          hamBtnLineParents[i].children[j].animate(
+            shrinkHamLineScaleKeyframes
+            ,hamLineAnimationOptionHalf
           );
-
-          hamBtnLineParents[2].children[j].animate(
-            closeHamburgerDownAnimation,
-            hamburgerAnimationOptionHalfDuration,
-          );
-
+          // --------------
           // ------- 2 ---------
 
           setTimeout(() => {
-            hamBtnLineParents[i].children[j].animate(
-              closeHamburgerScaleAnimation,
-              hamburgerAnimationOptionHalfDuration,
+            // 角度 -----
+            hamBtnLineParents[i].children[j].style.rotate = "0deg";
+            // ---------
+
+            console.log(hamBtnLineParents[i].children[j].style.scale, i, j);
+            hamBtnLineParents[0].children[j].animate(
+              moveUpHamLineKeyframesClose,
+              hamLineAnimationOptionHalf,
+            );
+
+            hamBtnLineParents[2].children[j].animate(
+              moveDownHamLineKeyframesClose,
+              hamLineAnimationOptionHalf,
             );
           }, hamOpDuraTime / 2);
 
           // ------- 3 ---------
+          setTimeout(() => {
+            hamBtnLineParents[i].children[j].animate(
+              extendHamLineScaleKeyframes,
+              hamLineAnimationOptionHalf,
+            );
+          }, hamOpDuraTime / 2);
+
+          
 
           // setTimeout(() => {
           //  // top & bottom のscale
@@ -172,7 +207,7 @@ const hamburgerAnimationOptionHalfDuration = {
       }
     }
   }
-}
+
 //----
 let canClickBtnFlg = true;
 hamburgerButton.addEventListener("click", () => {
@@ -180,7 +215,7 @@ hamburgerButton.addEventListener("click", () => {
   //   {
 
   //   }
-  //   ,hamburgerAnimationOption
+  //   ,hamLineAnimationOption
   // );
 
   if (canClickBtnFlg) {
@@ -200,7 +235,7 @@ hamburgerButton.addEventListener("click", () => {
     console.log(comStyleHamMenu.display);
     setTimeout(() => {
       canClickBtnFlg = true;
-    }, hamOpDuraTime * 2);
+    }, hamOpDuraTime * 1.5);
   }
 });
 /*
